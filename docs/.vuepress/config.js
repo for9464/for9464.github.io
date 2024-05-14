477e135ee04db3eb1074f655fa97ac2eba340898
only9464 <sky9464@qq.com>
2024-05-14 21:41:55 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+import { defineUserConfig } from 'vuepress'
+import { viteBundler } from '@vuepress/bundler-vite'
+import { plumeTheme } from 'vuepress-theme-plume'
+
+export default defineUserConfig({
+  base: '/blogs/',
+  title: '沙漠渔夫',
+  // 请不要忘记设置默认语言
+  lang: 'zh-CN',
+  theme: plumeTheme({
+    // more...
+    avatar: {
+      url: 'https://q1.qlogo.cn/g?b=qq&nk=2099311682&s=100',
+      name: 'only9464',
+      description: '一只蔡坤',
+      circle: true, // 是否为圆形头像
+    },
+    navbar: [
+      { text: '首页', link: '/11', icon: 'material-symbols:home-outline' },
+      { text: '博客', link: '/', icon: 'material-symbols:article-outline' },
+    ]
+  }),
+  
+  bundler: viteBundler(),
+})
